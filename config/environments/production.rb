1fdcb96c4671a3682ef9ac9fccbb16005eb13852
Jose Miguel Carbonell <jose.carbonell@robe5.com>
2010-08-09 10:09:55 +0200

    am config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Forums::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options[:host] = "forums-dev.heroku.com"
+  config.action_mailer.default_url_options = { :host => "forums-dev.heroku.com" }
 
   # Enable threaded mode
   # config.threadsafe!
