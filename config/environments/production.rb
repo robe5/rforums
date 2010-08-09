3f89e7650ff5124b27d61576a6918f83628ab768
Jose Miguel Carbonell <jose.carbonell@robe5.com>
2010-08-09 09:35:34 +0200

    adding sendgrid to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,3 +40,12 @@ Forums::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address	=> "smtp.sendgrid.net",
+  :port		=> "25",
+  :authentication => :plain,
+  :user_name    => ENV['SENDGRID_USERNAME'],
+  :password     => ENV['SENDGRID_PASSWORD'],
+  :domain       => ENV['SENDGRID_DOMAIN']
+}
