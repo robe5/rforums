0d1f21375cb3f2e0ef690fff707d98ee11365f30
Jose Miguel Carbonell <jose.carbonell@robe5.com>
2010-12-30 10:37:28 +0100

    adding debugger line

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -2,6 +2,7 @@ require 'bcrypt'
 #config = YAML.load_file(Rails.root.join('config', 'database.yml'))
 #MongoMapper.setup(config, Rails.env, {:logger => Rails.logger, :passenger => false})
 config = YAML.load_file(Rails.root.join('config', 'mongo.yml'))[Rails.env]
+Rails.logger.info(config.inspect)
 MongoMapper.connection = Mongo::Connection.new(config['host'], config['port'], {:logger => Rails.logger})
 MongoMapper.database = config['database']
 
