de0573645ba15fd80fb37ed1f74d6760e3393357
Jose M. Carbonell <robe5.agf@gmail.com>
2011-02-21 22:26:01 +0100

    production mongo config using env vars

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,7 +1,8 @@
 require 'bcrypt'
 #config = YAML.load_file(Rails.root.join('config', 'database.yml'))
 #MongoMapper.setup(config, Rails.env, {:logger => Rails.logger, :passenger => false})
-config = YAML.load_file(Rails.root.join('config', 'mongo.yml'))[Rails.env]
+config = YAML.load(ERB.new(File.read(Rails.root.join('config', 'mongo.yml'))).result)[Rails.env]
+#config = YAML.load_file(Rails.root.join('config', 'mongo.yml'))[Rails.env]
 MongoMapper.connection = Mongo::Connection.new(config['host'], config['port'], {:logger => Rails.logger})
 MongoMapper.database = config['database']
 
